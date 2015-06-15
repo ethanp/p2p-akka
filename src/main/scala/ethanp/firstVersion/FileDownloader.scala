9fade6adbe073f065be5041dd0f42cde36fa7891
ethanp <ethanp@utexas.edu>
2015-06-14 22:41:52 -0700

    needed another class. trying not to get too complex with it

--- src/main/scala/ethanp/firstVersion/FileDownloader.scala
+++ src/main/scala/ethanp/firstVersion/FileDownloader.scala
@@ -6,13 +6,13 @@ import akka.actor._
 import akka.event.LoggingReceive
 import ethanp.file.{FileToDownload, LocalP2PFile}
 
+import scala.collection.immutable.SortedSet
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor with ActorLogging {
-    val filename = fileDLing.fileInfo.filename
-    val numChunks = fileDLing.fileInfo.numChunks
+    import fileDLing.fileInfo._ // <- that's really cool
 
     val localFile = new File(downloadDir, filename)
     if (localFile.exists()) {
@@ -31,15 +31,47 @@ class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor
 
     var liveSeeders: Set[ActorRef] = fileDLing.seeders // TODO start out empty and fill with Pings
 
-    val chunkDownloaders = mutable.Set.empty[ActorRef]
+    var livePeers = Vector.empty[FilePeer] // TODO use livePeers.max to get the next one to DL from
 
-    var nextPosInt = 0
-    def nextSeeder: ActorRef = {
-        nextPosInt = (nextPosInt + 1) % Integer.MAX_VALUE
-        liveSeeders.toList(nextPosInt % liveSeeders.size)
+    /** They're ordered by how desirable they are to download from */
+    sealed abstract class FilePeer(val actorRef: ActorRef) extends Ordered[FilePeer] {
+        val UNKNOWN = -1
+        var histSpeedBytesSec: Int = UNKNOWN
+        var ongoingChunkTransfers = List.empty[ChunkDownloader]
+        def hasChunk(idx: Int): Boolean
+
+        // must ensure that lower == HIGHER priority
+        def priorityScore: Int =
+            if (ongoingChunkTransfers.size > 5) 0
+            else -histSpeedBytesSec
+
+        // Still naive? Here, we satisfy transitivity, reflexivity, etc. This must be preserved.
+        override def compare(that: FilePeer): Int = {
+            val CHOOSE_ME = -1
+            val TIE = 0
+            val CHOOSE_THEM = 1
+            (histSpeedBytesSec, that.histSpeedBytesSec) match {
+                case (UNKNOWN, UNKNOWN) => TIE
+                case (UNKNOWN, _) => CHOOSE_ME
+                case (_, UNKNOWN) => CHOOSE_THEM
+                case _ => priorityScore - that.priorityScore
+            }
+        }
     }
 
-    val incompleteChunks = mutable.BitSet(1 to numChunks:_*)
+    class Seeder(actorRef: ActorRef) extends FilePeer(actorRef) {
+        override def hasChunk(idx: Int): Boolean = true
+    }
+
+    class Leecher(actorRef: ActorRef) extends FilePeer(actorRef) {
+        val avblChunks = new mutable.BitSet(numChunks) // starts out as all zeros
+        override def hasChunk(idx: Int): Boolean = avblChunks.contains(idx)
+    }
+
+    val chunkDownloaders = mutable.Set.empty[ActorRef]
+
+
+    val incompleteChunks = mutable.BitSet(1 to numChunks:_*) // starts out as all ones
     val notStartedChunks = mutable.BitSet(1 to numChunks:_*)
 
     def addChunkDownload(): Unit = {
@@ -47,7 +79,7 @@ class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor
         if (notStartedChunks.nonEmpty) {
             val nextIdx = notStartedChunks.head
             notStartedChunks.remove(nextIdx)
-            downloadChunkFrom(nextIdx, nextSeeder)
+            downloadChunkFrom(nextIdx, nextToDLFrom(nextIdx))
         }
         else if (incompleteChunks.nonEmpty) {
             log.info("just waiting on transfers to complete")
@@ -59,6 +91,8 @@ class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor
         }
     }
 
+    def nextToDLFrom(nextIdx: Int): ActorRef = livePeers.filter(_ hasChunk nextIdx).max.actorRef
+
     def downloadChunkFrom(chunkIdx: Int, peerRef: ActorRef): Unit = {
         chunkDownloaders += context.actorOf(
             Props(classOf[ChunkDownloader], p2PFile, chunkIdx, peerRef),
@@ -69,7 +103,7 @@ class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor
     /** called by Akka framework when this Actor is asynchronously started */
     override def preStart(): Unit = {
         // TODO should be pinging potentialDownloadees instead!
-        (1 to 4).foreach(i => addChunkDownload())
+        potentialDownloadees.foreach(_ ! Ping())
     }
 
     /* speed calculations */
@@ -81,15 +115,19 @@ class FileDownloader(fileDLing: FileToDownload, downloadDir: File) extends Actor
 
     override def receive: Actor.Receive = LoggingReceive {
         case ChunkComplete(idx) =>
-            incompleteChunks(idx) = true
+            incompleteChunks.remove(idx)
             addChunkDownload()
 
         // this is received *after* the ChunkDownloader tried retrying a few times
         case ChunkDLFailed(idx, peerRef) =>
             liveSeeders -= peerRef
-            if (liveSeeders.nonEmpty) downloadChunkFrom(idx, nextSeeder)
+            // TODO update the FilePeer object
+            if (liveSeeders.nonEmpty) downloadChunkFrom(idx, nextToDLFrom(idx))
             else log.warning(s"$filename seederList is empty")
 
         case DownloadSpeed(numBytes) => bytesDLedPastSecond += numBytes // should be child-actor
+
+        // TODO this is where we should be calling addChunkDownload()
+        case Ping(_) => ???
     }
 }
